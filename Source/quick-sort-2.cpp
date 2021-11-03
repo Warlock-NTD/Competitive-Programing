51a91947212eaad7c956f8787d4eceb164fd6f81
Stahl <nguyenthanhduy030898@gmail.com>
2021-11-03 21:50:31 +0700

    .

--- Source/quick-sort-2.cpp
+++ Source/quick-sort-2.cpp
@@ -1,17 +1,36 @@
-// Ascending Order
-void quick_sort(int p ,vector<int> &c)
+#include<bits/stdc++.h>
+using namespace std;
+
+inline vector<int> quick_sort(int p, vector<int> ref_)
 {
-    vector<int> lft,rgt,mid;
-    for(auto &i :c)
+    if (ref_.size()==1) return ref_;
+    vector<int>left,right,mid;
+    for (auto &i : ref_)
     {
-        if (i<p)    lft.push_back(i);
-        else if(i>p)    rgt.push_back(i);
+        if(i < p)   left.push_back(i);
+        else if(i>p) right.push_back(i);
         else mid.push_back(i);
     }
-    if(!lft.empty())    quick_sort(lft[0], lft);
-    if(!rgt.empty())    quick_sort(rgt[0], rgt);
-    c.clear();
-    c.insert(c.end(),lft.begin(),lft.end());
-    c.insert(c.end(),mid.begin(),mid.end());
-    c.insert(c.end(),rgt.begin(),rgt.end());
+    ref_.clear();
+    if(!left.empty())   left = quick_sort(left[0],left);
+    if(!right.empty())  right = quick_sort (right[0],right);
+    ref_.insert(ref_.end(),left.begin(),left.end());
+    ref_.insert(ref_.end(),mid.begin(),mid.end());
+    ref_.insert(ref_.end(),right.begin(),right.end());
+    return ref_;
+}
+int main(int argc, char* argv[])
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n; cin>>n;
+    vector<int> ar;
+    for (int i = 0 ; i < n ; ++i)
+    {
+        int ip; cin>>ip;    ar.push_back(ip);
+    }
+    // quicksort implementation
+    ar = quick_sort(ar[0],ar);
+    for(auto&i : ar)    cout<<i<<' ';
+    return 0;
 }
